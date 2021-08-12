b1a82a77c9210db0bf29b96ff93de3e892938e41
muliyanto <muliyantochandra@gmail.com>
2021-08-13 03:07:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde quibusdam repellat error sunt, quo ad.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
